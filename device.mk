756ed3409d209d2d6fa773c3ed4d4f0167871346
dkpost3 <dkpost3@lmt-it.ru>
2025-05-04 02:56:22 +0300

    delete XiaomiDolby

--- device.mk
+++ device.mk
@@ -7,20 +7,28 @@
 # Inherit from the proprietary version
 $(call inherit-product, vendor/xiaomi/dolby/dolby-vendor.mk)
 
-# DolbyManager
+# Audio Config
 PRODUCT_PACKAGES += \
     XiaomiDolby
 
 # Build codec2 packages
 PRODUCT_PACKAGES += \
     libavservices_minijail \
-    libavservices_minijail_vendor \
+    libavservices_minijail_vendor
+
+PRODUCT_PACKAGES += \
     libcodec2_hidl@1.2.vendor \
     libcodec2_soft_common.vendor
 
+# Media Dolby VNDK
+PRODUCT_PACKAGES += \
+    libstagefright_foundation-v33:64
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml
+
 # Configs
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
     $(LOCAL_PATH)/configs/dax/dax-default.xml:$(TARGET_COPY_OUT_VENDOR)/etc/dolby/dax-default.xml
 
 # Soong namespaces
